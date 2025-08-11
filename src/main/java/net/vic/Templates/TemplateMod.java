0a58885229854dc2cd225c8a94e5e91af281e9b5
Victor <victor.vo1706@gmail.com>
2025-08-11 10:49:17 +0700

    Default Template

--- src/main/java/net/vic/Templates/TemplateMod.java
+++ src/main/java/net/vic/Templates/TemplateMod.java
@@ -1,4 +1,4 @@
-package net.vic.Template;
+package net.vic.Templates;
 
 import com.mojang.logging.LogUtils;
 import net.minecraftforge.api.distmarker.Dist;
@@ -14,15 +14,15 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(TutorialMod.MOD_ID)
-public class TutorialMod
+@Mod(TemplateMod.MOD_ID)
+public class TemplateMod
 {
     // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "template";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    public TutorialMod(FMLJavaModLoadingContext context)
+    public TemplateMod(FMLJavaModLoadingContext context)
     {
         IEventBus modEventBus = context.getModEventBus();
 
